07a3fb7a72a285773eff7817ae4de873cc5237e4
Sayem Kamal <sayemkamal12@gmail.com>
2025-01-13 00:38:19 -0500

    Write sumAll function considering edge cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(low,high) {
+    if (low < 0 || high < 0){
+        return 'ERROR';
+    }
+    if(typeof low !== "number" || typeof high !== "number"){
+        return 'ERROR';
+    }
+    if(!Number.isInteger(low) || !Number.isInteger(high)){
+        return 'ERROR';
+    }
+    if(low > high){
+        temp = low;
+        low = high;
+        high = temp;
+    }
+    let sum = 0;
+    for(let i = low; i<=high;i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
