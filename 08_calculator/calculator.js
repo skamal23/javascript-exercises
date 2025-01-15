03122bbeb16b391eace0ec4ea937fe7b3f59e50e
Sayem Kamal <sayemkamal12@gmail.com>
2025-01-14 22:59:24 -0500

    Complete series of calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,33 @@
-const add = function() {
-	
+const add = function(first, second) {
+	return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total,currentElement) => {
+    return total + currentElement;
+  },0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total,currentElement) => {
+    return total * currentElement;
+  } ,1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a,b);
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  let sum = 1;
+  for(let i = num; i > 0; i--){
+    sum *= i;
+  }
+  return sum;
 	
 };
 
